85ba3aaf73be99468a1745f9e70a5b010d5c964b
believer <rickard.laurin@gmail.com>
2016-06-17 15:10:13 +0200

    Added ESLint and fixed errors

--- clearingNumbers.js
+++ clearingNumbers.js
@@ -1,676 +1,676 @@
 module.exports = [
   {
-    "bank": "ABN AMRO",
-    "ranges": [
+    bank: 'ABN AMRO',
+    ranges: [
       {
-        "min": 9090,
-        "max": 9099
+        min: 9090,
+        max: 9099
       }
     ]
   },
   {
-    "bank": "Avanza Bank",
-    "ranges": [
+    bank: 'Avanza Bank',
+    ranges: [
       {
-        "min": 9550,
-        "max": 9550
+        min: 9550,
+        max: 9550
       },
       {
-        "min": 9559,
-        "max": 9559
+        min: 9559,
+        max: 9559
       }
     ]
   },
   {
-    "bank": "Bank2",
-    "ranges": [
+    bank: 'Bank2',
+    ranges: [
       {
-        "min": 9230,
-        "max": 9230
+        min: 9230,
+        max: 9230
       }
     ]
   },
   {
-    "bank": "Calyon Bank",
-    "ranges": [
+    bank: 'Calyon Bank',
+    ranges: [
       {
-        "min": 9080,
-        "max": 9080
+        min: 9080,
+        max: 9080
       }
     ]
   },
   {
-    "bank": "Citibank",
-    "ranges": [
+    bank: 'Citibank',
+    ranges: [
       {
-        "min": 9040,
-        "max": 9049
+        min: 9040,
+        max: 9049
       }
     ]
   },
   {
-    "bank": "Coop Bank",
-    "ranges": [
+    bank: 'Coop Bank',
+    ranges: [
       {
-        "min": 9290,
-        "max": 9299
+        min: 9290,
+        max: 9299
       }
     ]
   },
   {
-    "bank": "Danske Bank",
-    "ranges": [
+    bank: 'Danske Bank',
+    ranges: [
       {
-        "min": 1200,
-        "max": 1399
+        min: 1200,
+        max: 1399
       },
       {
-        "min": 2400,
-        "max": 2499
+        min: 2400,
+        max: 2499
       }
     ]
   },
   {
-    "bank": "Den Norske Bank",
-    "ranges": [
+    bank: 'Den Norske Bank',
+    ranges: [
       {
-        "min": 9190,
-        "max": 9199
+        min: 9190,
+        max: 9199
       }
     ]
   },
   {
-    "bank": "Ekobanken",
-    "ranges": [
+    bank: 'Ekobanken',
+    ranges: [
       {
-        "min": 9548,
-        "max": 9548
+        min: 9548,
+        max: 9548
       }
     ]
   },
   {
-    "bank": "Färs & Frosta Sparbank",
-    "ranges": [
+    bank: 'Färs & Frosta Sparbank',
+    ranges: [
       {
-        "min": "8079-6",
-        "max": "8079-6"
+        min: '8079-6',
+        max: '8079-6'
       }
     ]
   },
   {
-    "bank": "Forex Bank",
-    "ranges": [
+    bank: 'Forex Bank',
+    ranges: [
       {
-        "min": 9400,
-        "max": 9449
+        min: 9400,
+        max: 9449
       }
     ]
   },
   {
-    "bank": "GE Money Bank",
-    "ranges": [
+    bank: 'GE Money Bank',
+    ranges: [
       {
-        "min": 9460,
-        "max": 9460
+        min: 9460,
+        max: 9460
       },
       {
-        "min": 9469,
-        "max": 9469
+        min: 9469,
+        max: 9469
       }
     ]
   },
   {
-    "bank": "Gjensidige NOR Sparebank",
-    "ranges": [
+    bank: 'Gjensidige NOR Sparebank',
+    ranges: [
       {
-        "min": 9260,
-        "max": 9269
+        min: 9260,
+        max: 9269
       }
     ]
   },
   {
-    "bank": "Handelsbanken",
-    "ranges": [
+    bank: 'Handelsbanken',
+    ranges: [
       {
-        "min": 6000,
-        "max": 6999
+        min: 6000,
+        max: 6999
       }
     ]
   },
   {
-    "bank": "HSB Bank",
-    "ranges": [
+    bank: 'HSB Bank',
+    ranges: [
       {
-        "min": 9050,
-        "max": 9059
+        min: 9050,
+        max: 9059
       }
     ]
   },
   {
-    "bank": "ICA Banken",
-    "ranges": [
+    bank: 'ICA Banken',
+    ranges: [
       {
-        "min": 9270,
-        "max": 9279
+        min: 9270,
+        max: 9279
       }
     ]
   },
   {
-    "bank": "Ikano Bank",
-    "ranges": [
+    bank: 'Ikano Bank',
+    ranges: [
       {
-        "min": 9170,
-        "max": 9179
+        min: 9170,
+        max: 9179
       }
     ]
   },
   {
-    "bank": "JAK Medlemsbank",
-    "ranges": [
+    bank: 'JAK Medlemsbank',
+    ranges: [
       {
-        "min": 9549,
-        "max": 9549
+        min: 9549,
+        max: 9549
       }
     ]
   },
   {
-    "bank": "JP Nordiska",
-    "ranges": [
+    bank: 'JP Nordiska',
+    ranges: [
       {
-        "min": 2300,
-        "max": 2309
+        min: 2300,
+        max: 2309
       },
       {
-        "min": 2311,
-        "max": 2399
+        min: 2311,
+        max: 2399
       }
     ]
   },
   {
-    "bank": "Länsförsäkringar Bank",
-    "ranges": [
+    bank: 'Länsförsäkringar Bank',
+    ranges: [
       {
-        "min": 3400,
-        "max": 3409
+        min: 3400,
+        max: 3409
       },
       {
-        "min": 9020,
-        "max": 9029
+        min: 9020,
+        max: 9029
       },
       {
-        "min": 9060,
-        "max": 9069
+        min: 9060,
+        max: 9069
       }
     ]
   },
   {
-    "bank": "Lönneberga-Tuna-Vena Sparbank",
-    "ranges": [
+    bank: 'Lönneberga-Tuna-Vena Sparbank',
+    ranges: [
       {
-        "min": "8393-1",
-        "max": "8393-1"
+        min: '8393-1',
+        max: '8393-1'
       }
     ]
   },
   {
-    "bank": "Nordea",
-    "ranges": [
+    bank: 'Nordea',
+    ranges: [
       {
-        "min": 1100,
-        "max": 1199
+        min: 1100,
+        max: 1199
       },
       {
-        "min": 1400,
-        "max": 2099
+        min: 1400,
+        max: 2099
       },
       {
-        "min": 3000,
-        "max": 3299
+        min: 3000,
+        max: 3299
       },
       {
-        "min": 3301,
-        "max": 3399
+        min: 3301,
+        max: 3399
       },
       {
-        "min": 3410,
-        "max": 3781
+        min: 3410,
+        max: 3781
       },
       {
-        "min": 3783,
-        "max": 4999
+        min: 3783,
+        max: 4999
       }
     ]
   },
   {
-    "bank": "Nordea Personkonto",
-    "ranges": [
+    bank: 'Nordea Personkonto',
+    ranges: [
       {
-        "min": 3300,
-        "max": 3300
+        min: 3300,
+        max: 3300
       },
       {
-        "min": 3782,
-        "max": 3782
+        min: 3782,
+        max: 3782
       }
     ]
   },
   {
-    "bank": "Nordnet Bank",
-    "ranges": [
+    bank: 'Nordnet Bank',
+    ranges: [
       {
-        "min": 9100,
-        "max": 9100
+        min: 9100,
+        max: 9100
       }
     ]
   },
   {
-    "bank": "Plusgirot Bank",
-    "ranges": [
+    bank: 'Plusgirot Bank',
+    ranges: [
       {
-        "min": 9500,
-        "max": 9547
+        min: 9500,
+        max: 9547
       },
       {
-        "min": 9960,
-        "max": 9969
+        min: 9960,
+        max: 9969
       }
     ]
   },
   {
-    "bank": "Resurs Bank",
-    "ranges": [
+    bank: 'Resurs Bank',
+    ranges: [
       {
-        "min": 9280,
-        "max": 9289
+        min: 9280,
+        max: 9289
       }
     ]
   },
   {
-    "bank": "Sala Sparbank",
-    "ranges": [
+    bank: 'Sala Sparbank',
+    ranges: [
       {
-        "min": 8284,
-        "max": 8284
+        min: 8284,
+        max: 8284
       }
     ]
   },
   {
-    "bank": "SalusAnsvar Bank",
-    "ranges": [
+    bank: 'SalusAnsvar Bank',
+    ranges: [
       {
-        "min": 9231,
-        "max": 9239
+        min: 9231,
+        max: 9239
       }
     ]
   },
   {
-    "bank": "Sambox",
-    "ranges": [
+    bank: 'Sambox',
+    ranges: [
       {
-        "min": 2950,
-        "max": 2950
+        min: 2950,
+        max: 2950
       }
     ]
   },
   {
-    "bank": "SEB",
-    "ranges": [
+    bank: 'SEB',
+    ranges: [
       {
-        "min": 5000,
-        "max": 5999
+        min: 5000,
+        max: 5999
       },
       {
-        "min": 9120,
-        "max": 9124
+        min: 9120,
+        max: 9124
       },
       {
-        "min": 9130,
-        "max": 9149
+        min: 9130,
+        max: 9149
       }
     ]
   },
   {
-    "bank": "Skandiabanken",
-    "ranges": [
+    bank: 'Skandiabanken',
+    ranges: [
       {
-        "min": 9150,
-        "max": 9169
+        min: 9150,
+        max: 9169
       }
     ]
   },
   {
-    "bank": "Sparbanken i Enköping",
-    "ranges": [
+    bank: 'Sparbanken i Enköping',
+    ranges: [
       {
-        "min": 7121,
-        "max": 7122
+        min: 7121,
+        max: 7122
       },
       {
-        "min": "8305-5",
-        "max": "8305-5"
+        min: '8305-5',
+        max: '8305-5'
       }
     ]
   },
   {
-    "bank": "Sparbanken 1826",
-    "ranges": [
+    bank: 'Sparbanken 1826',
+    ranges: [
       {
-        "min": "8313-9",
-        "max": "8313-9"
+        min: '8313-9',
+        max: '8313-9'
       }
     ]
   },
   {
-    "bank": "Sparbanken Alingsås",
-    "ranges": [
+    bank: 'Sparbanken Alingsås',
+    ranges: [
       {
-        "min": "8304-8",
-        "max": "8304-8"
+        min: '8304-8',
+        max: '8304-8'
       }
     ]
   },
   {
-    "bank": "Sparbanken Nord",
-    "ranges": [
+    bank: 'Sparbanken Nord',
+    ranges: [
       {
-        "min": 8264,
-        "max": 8264
+        min: 8264,
+        max: 8264
       }
     ]
   },
   {
-    "bank": "Sparbanken Öresund",
-    "ranges": [
+    bank: 'Sparbanken Öresund',
+    ranges: [
       {
-        "min": 9300,
-        "max": 9300
+        min: 9300,
+        max: 9300
       }
     ]
   },
   {
-    "bank": "Stadshypotek Bank",
-    "ranges": [
+    bank: 'Stadshypotek Bank',
+    ranges: [
       {
-        "min": 9200,
-        "max": 9209
+        min: 9200,
+        max: 9209
       }
     ]
   },
   {
-    "bank": "Swedbank",
-    "ranges": [
+    bank: 'Swedbank',
+    ranges: [
       {
-        "min": 8000,
-        "max": 8999
+        min: 8000,
+        max: 8999
       },
       {
-        "min": 7000,
-        "max": 7999
+        min: 7000,
+        max: 7999
       },
       {
-        "min": 7123,
-        "max": 8104
+        min: 7123,
+        max: 8104
       },
       {
-        "min": "8383-2",
-        "max": "8383-2"
+        min: '8383-2',
+        max: '8383-2'
       },
       {
-        "min": "8006-9",
-        "max": "8006-9"
+        min: '8006-9',
+        max: '8006-9'
       },
       {
-          "min": "8018-4",
-          "max": "8018-4"
+        min: '8018-4',
+        max: '8018-4'
       },
       {
-          "min": "8021-8",
-          "max": "8021-8"
+        min: '8021-8',
+        max: '8021-8'
       },
       {
-          "min": "8024-2",
-          "max": "8024-2"
+        min: '8024-2',
+        max: '8024-2'
       },
       {
-          "min": "8030-9",
-          "max": "8030-9"
+        min: '8030-9',
+        max: '8030-9'
       },
       {
-          "min": "8032-5",
-          "max": "8032-5"
+        min: '8032-5',
+        max: '8032-5'
       },
       {
-          "min": "8050-7",
-          "max": "8050-7"
+        min: '8050-7',
+        max: '8050-7'
       },
       {
-          "min": "8059-8",
-          "max": "8059-8"
+        min: '8059-8',
+        max: '8059-8'
       },
       {
-          "min": "8060-6",
-          "max": "8060-6"
+        min: '8060-6',
+        max: '8060-6'
       },
       {
-          "min": "8077-0",
-          "max": "8077-0"
+        min: '8077-0',
+        max: '8077-0'
       },
       {
-          "min": "8079-6",
-          "max": "8079-6"
+        min: '8079-6',
+        max: '8079-6'
       },
       {
-          "min": "8103-4",
-          "max": "8103-4"
+        min: '8103-4',
+        max: '8103-4'
       },
       {
-          "min": "8105-9",
-          "max": "8105-9"
+        min: '8105-9',
+        max: '8105-9'
       },
       {
-          "min": "8114-1",
-          "max": "8114-1"
+        min: '8114-1',
+        max: '8114-1'
       },
       {
-          "min": "8129-9",
-          "max": "8129-9"
+        min: '8129-9',
+        max: '8129-9'
       },
       {
-          "min": "8138-0",
-          "max": "8138-0"
+        min: '8138-0',
+        max: '8138-0'
       },
       {
-          "min": "8143-0",
-          "max": "8143-0"
+        min: '8143-0',
+        max: '8143-0'
       },
       {
-          "min": "8147-1",
-          "max": "8147-1"
+        min: '8147-1',
+        max: '8147-1'
       },
       {
-          "min": "8150-5",
-          "max": "8150-5"
+        min: '8150-5',
+        max: '8150-5'
       },
       {
-          "min": "8153-9",
-          "max": "8153-9"
+        min: '8153-9',
+        max: '8153-9'
       },
       {
-          "min": "8156-2",
-          "max": "8156-2"
+        min: '8156-2',
+        max: '8156-2'
       },
       {
-          "min": "8166-1",
-          "max": "8166-1"
+        min: '8166-1',
+        max: '8166-1'
       },
       {
-          "min": "8169-5",
-          "max": "8169-5"
+        min: '8169-5',
+        max: '8169-5'
       },
       {
-          "min": "8177-7",
-          "max": "8177-7"
+        min: '8177-7',
+        max: '8177-7'
       },
       {
-          "min": "8182-8",
-          "max": "8182-8"
+        min: '8182-8',
+        max: '8182-8'
       },
       {
-          "min": "8183-6",
-          "max": "8183-6"
+        min: '8183-6',
+        max: '8183-6'
       },
       {
-          "min": "8187-7",
-          "max": "8187-7"
+        min: '8187-7',
+        max: '8187-7'
       },
       {
-          "min": "8201-6",
-          "max": "8201-6"
+        min: '8201-6',
+        max: '8201-6'
       },
       {
-          "min": "8214-9",
-          "max": "8214-9"
+        min: '8214-9',
+        max: '8214-9'
       },
       {
-          "min": "8222-2",
-          "max": "8222-2"
+        min: '8222-2',
+        max: '8222-2'
       },
       {
-          "min": "8239-6",
-          "max": "8239-6"
+        min: '8239-6',
+        max: '8239-6'
       },
       {
-          "min": "8240-4",
-          "max": "8240-4"
+        min: '8240-4',
+        max: '8240-4'
       },
       {
-          "min": "8242-0",
-          "max": "8242-0"
+        min: '8242-0',
+        max: '8242-0'
       },
       {
-          "min": "8257-8",
-          "max": "8257-8"
+        min: '8257-8',
+        max: '8257-8'
       },
       {
-          "min": "8264-4",
-          "max": "8264-4"
+        min: '8264-4',
+        max: '8264-4'
       },
       {
-          "min": "8270-1",
-          "max": "8270-1"
+        min: '8270-1',
+        max: '8270-1'
       },
       {
-          "min": "8284-2",
-          "max": "8284-2"
+        min: '8284-2',
+        max: '8284-2'
       },
       {
-          "min": "8299-0",
-          "max": "8299-0"
+        min: '8299-0',
+        max: '8299-0'
       },
       {
-          "min": "8304-8",
-          "max": "8304-8"
+        min: '8304-8',
+        max: '8304-8'
       },
       {
-          "min": "8305-5",
-          "max": "8305-5"
+        min: '8305-5',
+        max: '8305-5'
       },
       {
-          "min": "8313-9",
-          "max": "8313-9"
+        min: '8313-9',
+        max: '8313-9'
       },
       {
-          "min": "8314-7",
-          "max": "8314-7"
+        min: '8314-7',
+        max: '8314-7'
       },
       {
-          "min": "8327-9",
-          "max": "8327-9"
+        min: '8327-9',
+        max: '8327-9'
       },
       {
-          "min": "8346-9",
-          "max": "8346-9"
+        min: '8346-9',
+        max: '8346-9'
       },
       {
-          "min": "8353-5",
-          "max": "8353-5"
+        min: '8353-5',
+        max: '8353-5'
       },
       {
-          "min": "8354-3",
-          "max": "8354-3"
+        min: '8354-3',
+        max: '8354-3'
       },
       {
-          "min": "8368-3",
-          "max": "8368-3"
+        min: '8368-3',
+        max: '8368-3'
       },
       {
-          "min": "8381-6",
-          "max": "8381-6"
+        min: '8381-6',
+        max: '8381-6'
       },
       {
-          "min": "8393-1",
-          "max": "8393-1"
+        min: '8393-1',
+        max: '8393-1'
       },
       {
-          "min": "8401-2",
-          "max": "8401-2"
+        min: '8401-2',
+        max: '8401-2'
       },
       {
-          "min": "8403-8",
-          "max": "8403-8"
+        min: '8403-8',
+        max: '8403-8'
       },
       {
-          "min": "8405-3",
-          "max": "8405-3"
+        min: '8405-3',
+        max: '8405-3'
       },
       {
-          "min": "8417-8",
-          "max": "8417-8"
+        min: '8417-8',
+        max: '8417-8'
       },
       {
-          "min": "8420-2",
-          "max": "8420-2"
+        min: '8420-2',
+        max: '8420-2'
       },
       {
-          "min": "8431-9",
-          "max": "8431-9"
+        min: '8431-9',
+        max: '8431-9'
       },
       {
-          "min": "8434-3",
-          "max": "8434-3"
+        min: '8434-3',
+        max: '8434-3'
       },
       {
-          "min": "8440-0",
-          "max": "8440-0"
+        min: '8440-0',
+        max: '8440-0'
       },
       {
-          "min": "8452-5",
-          "max": "8452-5"
+        min: '8452-5',
+        max: '8452-5'
       },
       {
-          "min": "8464-0",
-          "max": "8464-0"
+        min: '8464-0',
+        max: '8464-0'
       },
       {
-          "min": "8480-6",
-          "max": "8480-6"
+        min: '8480-6',
+        max: '8480-6'
       },
       {
-          "min": "8860-9",
-          "max": "8860-9"
+        min: '8860-9',
+        max: '8860-9'
       },
       {
-          "min": "8901-1",
-          "max": "8901-1"
+        min: '8901-1',
+        max: '8901-1'
       }
     ]
   },
   {
-    "bank": "Ålandsbanken",
-    "ranges": [
+    bank: 'Ålandsbanken',
+    ranges: [
       {
-        "min": 2310,
-        "max": 2310
+        min: 2310,
+        max: 2310
       }
     ]
   }
--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-var _ = require('lodash');
-var banks = require('./clearingNumbers');
+var _ = require('lodash')
+var banks = require('./clearingNumbers')
 
 function bankName (number) {
-  number = number.toString();
+  number: string = number.toString()
 
   var inRange = _.map(banks, function (bank) {
-      var filter = _.filter(bank.ranges, function (range) {
-        var min = range.min.toString();
-        var max = range.max.toString();
+    var filter = _.filter(bank.ranges, function (range) {
+      var min = range.min.toString()
+      var max = range.max.toString()
 
-        return _.inRange(number, min, max) || (min === number && max === number);
-      });
-
-      if (filter.length) {
-        return bank;
-      }
+      return _.inRange(number, min, max) || (min === number && max === number)
     })
 
-  var results = _.first(_.compact(inRange));
-  return results ? results.bank : '';
+    if (filter.length) {
+      return bank
+    }
+  })
+
+  var results = _.first(_.compact(inRange))
+  return results ? results.bank : ''
 }
 
 function clearingNumbers (bankName) {
   return _.find(banks, function (bank) {
-    return bank.bank === bankName;
-  }).ranges;
+    return bank.bank === bankName
+  }).ranges
 }
 
 function allBanks () {
   return banks.map(function (bank) {
     return bank.bank
-  });
+  })
 }
 
 module.exports = {
   bankName: bankName,
   clearingNumbers: clearingNumbers,
   allBanks: allBanks
-};
+}
